5da43bff1dd0aff5faf210a143744d97d4d80b21
NIDHI THAKUR <THAKURNIDHI503@GMAIL.COM>
2021-07-02 19:15:22 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvcRegistrationPage.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace mvcRegistrationPage.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User 
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Registration(UserClass uc)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DBcon"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_RegisterData ";
+            cmd.Parameters.AddWithValue("@First_Name", uc.First_Name);
+            cmd.Parameters.AddWithValue("@Last_Name", uc.Last_Name);
+            cmd.Parameters.AddWithValue("@Email_id", uc.Email);
+            cmd.Parameters.AddWithValue("@Password", uc.Password);
+            cmd.Connection = conn;
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            
+            Session["FirstName"] = uc.First_Name.ToString();
+            return RedirectToAction("RegistrationDone", "User");         
+           
+        }
+        public ActionResult Cancel(UserClass uc)
+        {
+             ModelState.Clear();
+             return View();
+        }
+
+        public ActionResult RegistrationDone()
+        {
+            return View();
+            
+        }
+      
+
+
+    }
+}
---
+++ Models/UserClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using mvcRegistrationPage.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace mvcRegistrationPage.Models
+{
+    public class UserClass
+    {
+        [Required(ErrorMessage = "Enter First Name")]
+        [Display(Name = "First Name")]
+        public string First_Name { get; set; }
+
+        [Required(ErrorMessage = "Enter Last Name")]
+        [Display(Name = "Last Name")]
+        public string Last_Name { get; set; }
+
+        [Required(ErrorMessage = "Enter Email id")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid. Please enter in the form abc@xyx.com")]
+        [Display(Name = "Email id")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Enter Passsword")]
+        [Display(Name = "Password")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$", ErrorMessage = "Password must be between 8 and 15 characters and contain one uppercase letter, one lowercase letter, one digit and one special character.")]
+        //[StringLength(15, MinimumLength = 8, ErrorMessage = "Name must be between 8 and 15 char")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Enter the Password again")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string Confirm_Password { get; set; }
+
+    }
+}
